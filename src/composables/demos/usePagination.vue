9449df057d46c143c7fd1e82f7fd62ec600c8ea3
DPoroshin <dm.poroshin@gmail.com>
2025-11-26 10:14:19 +0300

    feat: usePagination

---
+++ src/composables/demos/usePagination.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { usePagination } from '@/composables';
+
+const pagination = usePagination<number>((page, size) => {
+  const DATA: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        items: DATA.slice((page - 1) * size, page * size),
+        page,
+        size,
+        total: DATA.length
+      });
+    }, 1000);
+  });
+});
+
+onMounted(() => {
+  pagination.first();
+});
+</script>
+
+<template>
+  <p>
+    Items: {{ pagination.items.value }}
+  </p>
+  <p>
+    Selected {{ pagination.page.value }}
+  </p>
+  <p>
+    Shown {{ `${pagination.items.value.length} of ${pagination.total.value}` }}
+  </p>
+  <nav v-if="pagination.hasPages.value">
+    <button
+        :disabled="pagination.isFirst.value"
+        @click="pagination.first"
+    >
+      First
+    </button>
+    <button
+        :disabled="!pagination.hasPrev.value"
+        @click="pagination.prev">
+      Prev
+    </button>
+    <button
+        v-for="page in pagination.count.value"
+        :key="page"
+        :disabled="page === pagination.page.value"
+        @click="pagination.page.value = page"
+    >
+      {{ page }}
+    </button>
+    <button
+        :disabled="!pagination.hasNext.value"
+        @click="pagination.next"
+    >
+      Next
+    </button>
+    <button
+        :disabled="pagination.isLast.value"
+        @click="pagination.last"
+    >
+      Last
+    </button>
+    <button
+        :disabled="!pagination.hasNext.value"
+        @click="pagination.append"
+    >
+      Append
+    </button>
+    <button @click="pagination.refresh">
+      Refresh
+    </button>
+
+    <select v-model="pagination.size.value">
+      <option
+          v-for="size in pagination.sizes.value"
+          :key="size"
+          :value="size"
+      >
+        {{ size }}
+      </option>
+    </select>
+  </nav>
+  <p v-if="pagination.isLoading.value">
+    Loading...
+  </p>
+</template>
---
+++ src/composables/usePagination.ts
@@ -0,0 +1,214 @@
+import {
+  ref,
+  type Ref,
+  readonly,
+  computed,
+  reactive,
+  watch,
+} from 'vue';
+import {
+  isFunction,
+} from '@vue/shared';
+
+
+type PaginatedData<T> = {
+    items: T[];
+    page: number;
+    size: number;
+    total: number;
+};
+
+// TODO: Add the limit + offset common signature
+// TODO: Add the no-promise overload signature
+type Source<T> =
+    | T[]
+    | ((page: number, size: number) => Promise<PaginatedData<T>>);
+
+type Options = {
+    page: number;
+    size: number;
+    sizes: number[];
+};
+
+const OPTIONS_DEFAULT: Readonly<Options> = {
+    page: 0,
+    size: 3,
+    sizes: [3, 5, 10],
+};
+
+// TODO: Add description and use cases
+export function usePagination<T>(source: Source<T>, options?: Options) {
+    const OPTIONS: Options = { ...OPTIONS_DEFAULT, ...options };
+
+    const items = ref<T[]>([]) as Ref<T[]>; // Inevitable `as` assertion :(
+
+    type Params = {
+        page: number;
+        size: number;
+    };
+
+    const batches = reactive<Params>({
+        page: OPTIONS.page,
+        size: OPTIONS.size,
+    });
+
+    const state = reactive<Params>({
+        page: OPTIONS.page,
+        size: OPTIONS.size,
+    });
+
+    const page = computed<number>({
+        get() {
+            return state.page;
+        },
+        set(page) {
+          Object.assign(batches, {
+              page,
+              size: state.size,
+          });
+        },
+    });
+
+    const size = computed<number>({
+        get() {
+            return state.size;
+        },
+        set(size){
+           Object.assign(batches, {
+               page: size !== state.size ? 1 : state.page,
+               size,
+           });
+        },
+    });
+
+    const sizes = ref<number[]>(OPTIONS.sizes);
+
+    const count = computed<number>(() => {
+        const count = Math.ceil(total.value / size.value);
+        return Number.isFinite(count) && count > 0 ? count : 0;
+    });
+
+    const total = ref<number>(0);
+
+    // TODO: Add the optional min timeout to prevent flickering
+    const isLoading = ref<boolean>(false);
+
+    const hasPages = computed<boolean>(() => {
+        return count.value > 1;
+    });
+
+    const hasPrev = computed<boolean>(() => {
+        return hasPages.value && page.value > 1;
+    });
+
+    const hasNext = computed<boolean>(() => {
+        return hasPages.value && page.value < count.value;
+    });
+
+    const isFirst = computed<boolean>(() => {
+        return hasPages.value && page.value === 1;
+    });
+
+    const isLast = computed<boolean>(() => {
+        return hasPages.value && page.value === count.value;
+    });
+
+    async function prev(): Promise<void> {
+        if (hasPrev.value) {
+           page.value = page.value - 1;
+        }
+    }
+
+    async function next(): Promise<void> {
+        if (hasNext.value) {
+          page.value = page.value + 1;
+        }
+    }
+
+    async function append(): Promise<void> {
+        const data = await execute({
+            page: state.page + 1,
+            size: state.size,
+        });
+
+        update({
+            page: data.page,
+            size: data.size,
+            items: items.value.concat(data.items),
+            total: data.total,
+        });
+    }
+
+    function first(): void {
+        page.value = 1;
+    }
+
+    function last(): void {
+        page.value = count.value;
+    }
+
+    async function refresh(): Promise<void> {
+        const data = await execute({
+            page: state.page,
+            size:  state.size,
+        });
+
+        update(data);
+    }
+
+    // TODO: Prevent promise racing
+    async function execute(payload: Params): Promise<PaginatedData<T>> {
+        if (isFunction(source)) {
+            try {
+                isLoading.value = true;
+                return await source(payload.page, payload.size);
+            } finally {
+                isLoading.value = false;
+            }
+        }
+
+        return {
+            items: source.slice((payload.page - 1) * payload.size, payload.page * payload.size),
+            page: payload.page,
+            size: payload.size,
+            total: source.length,
+        };
+    }
+
+    function update(params: { page: number, size: number, items: T[], total: number }): void {
+        state.page = params.page;
+        state.size = params.size;
+        items.value = params.items;
+        total.value = params.total;
+    }
+
+    watch(batches, async ({ page, size }) => {
+        const data = await execute({
+            page,
+            size,
+        });
+
+        update(data);
+    });
+
+    return {
+        items: readonly(items),
+        isLoading: readonly(isLoading),
+        page,
+        size,
+        sizes,
+        count,
+        total: readonly(total),
+        hasPages,
+        hasPrev,
+        hasNext,
+        isFirst,
+        isLast,
+        prev,
+        next,
+        first,
+        last,
+        append,
+        refresh,
+    }
+}
