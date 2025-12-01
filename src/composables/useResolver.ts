f9dc25b1b87f8470fcff355272dbcf9ecb2eb7cb
DPoroshin <dm.poroshin@gmail.com>
2025-12-01 12:27:46 +0300

    feat: useResolver.ts

--- src/composables/useResolver.ts
+++ src/composables/useResolver.ts
@@ -1,4 +1,28 @@
+import {
+  ref,
+  unref,
+  computed,
+  type MaybeRef,
+} from 'vue';
+
 // TODO: Implement it
-export function useResolver() {
-  throw new Error('useResolver must be implemented');
+export function useResolver<
+  const TKey extends string | number | symbol,
+  const TValue,
+  const TPredicate extends TKey
+>(
+  predicate: MaybeRef<TPredicate>,
+  resolver: Record<TKey, TValue>,
+) {
+  return computed<TValue>(() => {
+    return resolver[unref(predicate)];
+  });
 }
+
+const status = ref<'Success' | 'Error' | 'Unknown'>('Unknown');
+
+const result = useResolver(status, {
+  "Success": "OK",
+  "Error": "Not OK",
+  "Unknown": "We don't know",
+});
