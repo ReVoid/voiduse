60fa1473bbd5c4f5815f45216e6b21ce3106f652
DPoroshin <dm.poroshin@gmail.com>
2026-02-06 18:48:36 +0300

    docs: useDefault.ts docs

--- src/composables/useDefault.ts
+++ src/composables/useDefault.ts
@@ -1,46 +1,47 @@
-import {
-  ref,
-  unref,
-  type Ref,
-  type MaybeRef,
-  computed,
-} from 'vue';
+import { ref, unref, type Ref, type MaybeRef, computed } from 'vue';
 
-import {
-  isObject,
-  isUndefined,
-  isNull,
-} from '@sniptt/guards';
+import { isObject, isUndefined, isNull } from '@sniptt/guards';
 
-import {
-  merge,
-} from 'lodash-es';
+import { merge } from 'lodash-es';
 
-type ConsistentOutput<T> = T extends object
-  ? Required<T>
-  : NonNullable<T>;
-
+type ConsistentOutput<T> = T extends object ? Required<T> : NonNullable<T>;
 
 /**
- * TODO: Docs.
- * @param defaultValue
+ * Consistent object with default values and flexible API.
+ *
+ * @example
+ * ```ts
+ * // Putting default values
+ * const { item, reset } = useDefault({ name: 'Unknown', salary: 0 });
+ *
+ * // Changing normally
+ * item.value = { name: 'John', salary: 1000 };
+ *
+ * // Changing with blank values
+ * item.value = { name: 'Jane', salary: undefined }; // { name: 'Jane', salary: 0 }
+ *
+ * // Resetting
+ * reset(); // { name: 'Unknown', salary: 0 }
+ * ```
  */
 // TODO: Implement value argument as first argument.
 export function useDefault<T>(defaultValue: MaybeRef<ConsistentOutput<T>>) {
-  const _item = ref<ConsistentOutput<T>>(unref(defaultValue)) as Ref<ConsistentOutput<T>>
+  const _item = ref<ConsistentOutput<T>>(unref(defaultValue)) as Ref<ConsistentOutput<T>>;
 
   const item = computed<ConsistentOutput<T>, Partial<T>>({
     get() {
-      return _item.value
+      return _item.value;
     },
     set(payload) {
-      update(payload)
+      update(payload);
     },
-  })
+  });
 
   // TODO: Check with array type
   /**
    * Helper to avoid `computed` {@link https://github.com/vuejs/language-tools/issues/5793 issue} in `template` section.
+   *
+   * Do not use it within the `<script>` section.
    * @example
    * ```vue
    * <template>
@@ -72,5 +73,5 @@ export function useDefault<T>(defaultValue: MaybeRef<ConsistentOutput<T>>) {
     item,
     reset,
     update,
-  }
+  };
 }
