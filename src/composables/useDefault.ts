ffb3052420bc462026c66b7fdb8ee6102d7d2d3b
DPoroshin <dm.poroshin@gmail.com>
2026-02-06 19:14:50 +0300

    docs: useDefault.ts docs

--- src/composables/useDefault.ts
+++ src/composables/useDefault.ts
@@ -43,6 +43,24 @@ type ConsistentOutput<T> = T extends object
 export function useDefault<T>(defaultValue: MaybeRef<ConsistentOutput<T>>) {
   const _item = ref<ConsistentOutput<T>>(unref(defaultValue)) as Ref<ConsistentOutput<T>>;
 
+  /**
+   * Consistent value with default fallback.
+   *
+   * @example
+   * ```ts
+   * // Putting default values
+   * const { item, reset } = useDefault({ name: 'Unknown', salary: 0 });
+   *
+   * // Changing normally
+   * item.value = { name: 'John', salary: 1000 };
+   *
+   * // Changing with no explicit values
+   * item.value = { name: 'Jack' }; // { name: 'Jack', salary: 1000 }
+   *
+   * // Changing with blank values
+   * item.value = { name: 'Jane', salary: undefined }; // { name: 'Jane', salary: 0 }
+   * ```
+   */
   const item = computed<ConsistentOutput<T>, Partial<T>>({
     get() {
       return _item.value;
@@ -59,7 +77,13 @@ export function useDefault<T>(defaultValue: MaybeRef<ConsistentOutput<T>>) {
    * Do not use it within the `<script>` section.
    * @example
    * ```vue
+   * <script setup lang="ts">
+   *   const { update } = useDefault({ name: 'Unknown', salary: 0 });
+   *   const payload = { name: 'John' };
+   * </script>
+   *
    * <template>
+   *   <!-- { name: 'John', salary: 0 } -->
    *   <button @click="update(payload)">
    *     Update
    *   </button>
@@ -80,6 +104,18 @@ export function useDefault<T>(defaultValue: MaybeRef<ConsistentOutput<T>>) {
     _item.value = payload;
   }
 
+  /**
+   * Resets the value to the default one.
+   *
+   * @example
+   * ```ts
+   * const { item, reset } = useDefault({ name: 'Unknown', salary: 0 });
+   *
+   * item.value = { name: 'John', salary: 1000 };
+   *
+   * reset(); // { name: 'Unknown', salary: 0 }
+   * ```
+   */
   function reset(): void {
     update(defaultValue);
   }
