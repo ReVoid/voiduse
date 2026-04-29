d2e5568e7b1e5d81369291b58196f819e01a6ab0
DPoroshin <dm.poroshin@gmail.com>
2026-04-29 09:00:32 +0300

    test: useLoading.ts

---
+++ src/composables/useLoading/useLoading.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test, vitest, beforeEach, afterEach } from 'vitest';
+
+import { useLoading } from '@/composables';
+
+beforeEach(() => {
+  vitest.useFakeTimers();
+});
+
+afterEach(() => {
+  vitest.useRealTimers();
+});
+
+describe('useLoading', () => {
+  test('Changing state', async () => {
+    const { isLoading, showUntil } = useLoading();
+
+    showUntil(new Promise((resolve) => setTimeout(resolve, 1000)));
+
+    expect(isLoading.value).toBe(true);
+
+    await vitest.advanceTimersByTimeAsync(1000);
+
+    expect(isLoading.value).toBe(false);
+  });
+
+  test('Changing state manually is not allowed', async () => {
+    const { isLoading } = useLoading();
+
+    // @ts-expect-error: for testing purposes only.
+    isLoading.value = true;
+
+    expect(isLoading.value).toBe(false);
+  })
+});
